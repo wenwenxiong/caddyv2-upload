ac60952ee286cbea8956398ba13ab5c15c8dc73d
root <root@DESKTOP-KAMSL5R.localdomain>
2022-10-20 18:46:39 +0800

    fix import

--- upload.go
+++ upload.go
@@ -7,9 +7,11 @@ import (
 	"os"
 	"strconv"
 
+        "github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
 	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"github.com/dustin/go-humanize"
 	"go.uber.org/zap"
 )
 
